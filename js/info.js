c00dbc9e9710e876dfb82187cae5e2e23606d0a7
Martin Miller <milleronic@gmail.com>
2025-05-11 16:10:12 -0500

    css, info close js

--- js/info.js
+++ js/info.js
@@ -1,5 +1,10 @@
-	$(document).ready(function() {
-	$("body").on("click", "#close", function(){
+$(document).ready(function() {
+	$('body').on('click', '#close', function(){
 		$('.info').remove();
 	})
+	$(document).on('click', function(event){
+		if (!$(event.target).closest('.info, foreignObject' ).length && $('.info').length) {
+			$('.info').remove();
+		}
+	});
 })
